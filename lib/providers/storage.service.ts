14c36a5d3be2e7a133e2db7758252400a56a8b00
Roberto Colabene <robertocolabene@maior.team>
2024-07-15 17:58:52 +0200

    dev

--- lib/providers/storage.service.ts
+++ lib/providers/storage.service.ts
@@ -8,35 +8,40 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class StorageService {
   public rootPath = joinFromRoot('public')
+  public useRealPath = false
 
-  public async read(path:string): Promise<Buffer> {
-    const realPath = join(this.rootPath,path)
+  public async read(relativePath:string): Promise<Buffer> {
+    const realPath = this.realPath(relativePath)
     return await fs.readFile(realPath, {});
   }
 
-  public async write(path:string,data:string | NodeJS.ArrayBufferView | Iterable<string | NodeJS.ArrayBufferView> | AsyncIterable<string | NodeJS.ArrayBufferView> | internal.Stream): Promise<void> {
-    const realPath = join(this.rootPath,path)
+  public async write(relativePath:string,data:string | NodeJS.ArrayBufferView | Iterable<string | NodeJS.ArrayBufferView> | AsyncIterable<string | NodeJS.ArrayBufferView> | internal.Stream): Promise<void> {
+    const realPath = this.realPath(relativePath)
     await this.safeDirectory(realPath)
     return await fs.writeFile(realPath, data, 'utf8');
   }
 
-  public async append(path:string,data:string | Uint8Array): Promise<void> {
-    const realPath = join(this.rootPath,path)
+  public async append(relativePath:string,data:string | Uint8Array): Promise<void> {
+    const realPath = this.realPath(relativePath)
     await this.safeDirectory(realPath)
     return await fs.appendFile(realPath, data + EOL);
   }
 
-  public async delete(path:string): Promise<void> {
-    const realPath = join(this.rootPath,path)
+  public async delete(relativePath:string): Promise<void> {
+    const realPath = this.realPath(relativePath)
     try {
       return await fs.unlink(realPath)
     }
     catch(e){}
   }
 
-  public async stat(path:string): Promise<Stats> {
-    const realPath = join(this.rootPath,path)
+  public async stat(relativePath:string): Promise<Stats> {
+    const realPath = this.realPath(relativePath)
     return fs.stat(realPath)
+  }
+
+  private realPath(relativePath:string): string {
+    return this.useRealPath ? relativePath : join(this.rootPath,relativePath)
   }
 
   private async safeDirectory(path:string): Promise<void> {
