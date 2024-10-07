8942077630ab586e1ff7b3c75a17f439eda0aabd
Roberto Colabene <robertocolabene@maior.team>
2024-10-07 11:52:15 +0200

    dev

--- lib/pipes/image-compression.pipe.ts
+++ lib/pipes/image-compression.pipe.ts
@@ -8,7 +8,7 @@ export class ImageCompressionPipe implements PipeTransform<Express.Multer.File, 
     try {
       const compressedBuffer = await sharp(file.buffer).webp({ 
         quality: 70,
-      }).toBuffer()
+      }).withMetadata().toBuffer()
 
       file.buffer = compressedBuffer
       file.mimetype = 'image/webp'
