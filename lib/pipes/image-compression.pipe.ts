2786fac49e87c5ddc62de214600f90d96ab39a06
Sabatino Carotenuto <sabatinocarotenuto@maioradv.com>
2024-10-17 17:27:13 +0200

    prova

--- lib/pipes/image-compression.pipe.ts
+++ lib/pipes/image-compression.pipe.ts
@@ -10,7 +10,7 @@ export class ImageCompressionPipe implements PipeTransform<Express.Multer.File, 
   async transform(file: Express.Multer.File, metadata: ArgumentMetadata) {
     try {
       const compressedBuffer = await sharp(file.buffer).webp({ 
-        quality: 80,
+        quality: 100,
       }).rotate().toBuffer()
 
       file.buffer = compressedBuffer
