121ea20ee5d7ef0940f187ce4ed7203bbfc68ffc
Sabatino Carotenuto <sabatinocarotenuto@maioradv.com>
2024-10-21 10:24:55 +0200

    prova

--- lib/pipes/image-compression.pipe.ts
+++ lib/pipes/image-compression.pipe.ts
@@ -9,9 +9,34 @@ export class ImageCompressionPipe implements PipeTransform<Express.Multer.File, 
 
   async transform(file: Express.Multer.File, metadata: ArgumentMetadata) {
     try {
-      const compressedBuffer = await sharp(file.buffer).webp({ 
+      /*const compressedBuffer = await sharp(file.buffer).webp({ 
         quality: 70,
-      }).rotate().toBuffer()
+      }).rotate().toBuffer()*/
+
+      const image = sharp(file.buffer);
+      const metadata = await image.metadata();
+
+      const compressedBuffer = await image
+      .resize({
+        width: metadata.width > metadata.height ? 1920 : null,
+        height: metadata.height > metadata.width ? 1920 : null,
+        fit: 'inside',
+      })
+      .withMetadata() // Mantieni il profilo colore e i metadati dell'immagine originale
+      .webp({ quality: 90 }) // Alta qualità per mantenere la nitidezza
+      .rotate()
+      .toBuffer();
+
+
+      /*const thumbnailBuffer = await sharp(file.buffer)
+      .resize({
+        width: 128,
+        height: 300,
+        fit: 'cover', // Ritaglia l'immagine per adattarsi a 128x300 mantenendo le proporzioni
+        position: 'center', // Centra l'immagine durante il ritaglio
+      })
+      .webp({ quality: 70 })
+      .toBuffer();*/
 
       file.buffer = compressedBuffer
       file.mimetype = 'image/webp'
