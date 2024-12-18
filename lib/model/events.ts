85c099e0e428b1d1939101c8a590834e690810bb
Roberto Colabene <robertocolabene@maior.team>
2024-12-18 15:22:48 +0100

    dev

--- lib/model/events.ts
+++ lib/model/events.ts
@@ -3,21 +3,18 @@ export abstract class BaseEvent<B extends string,E extends string,P extends Reco
   constructor(public readonly payload: P) {}
 }
 
-export const buildEvents = <B extends string,Map extends Record<string,any>>(base: B,events:(keyof Map)[]): Events<B,Map> => {
+/*export const buildEvents = <B extends string,Map extends Record<string,any>>(base: B,events:(keyof Map)[]): Events<B,Map> => {
   return events.reduce((o,key) => {
     o[key] = {
       name:`${base}.${key as Extract<typeof key, string>}`,
       class: class extends BaseEvent<B,Extract<typeof key, string>, Map[typeof key]> {}
     }
   },{} as any)
-}
+}*/
 
 export type Events<
   B extends string,
   H extends Record<string,any>
 > = {
-  readonly [K in keyof H]: {
-    name: `${B}.${Extract<K, string>}`;
-    class: new (payload: H[K]) => BaseEvent<B, Extract<K, string>, H[K]>;
-  };
+  readonly [K in keyof H]: new (payload: H[K]) => BaseEvent<B, Extract<K, string>, H[K]>
 };
