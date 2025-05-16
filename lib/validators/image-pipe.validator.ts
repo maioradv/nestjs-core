7a0042ac8529a8273a37e539e203f7237ee93c7d
Roberto Colabene <robertocolabene@maior.team>
2025-05-16 14:46:57 +0200

    dev

--- lib/validators/image-pipe.validator.ts
+++ lib/validators/image-pipe.validator.ts
@@ -2,7 +2,7 @@ import { HttpStatus, ParseFilePipeBuilder } from "@nestjs/common";
 
 export const DEFAULT_MAX_IMAGE_SIZE = 10;
 //heic uploaded as application/octet-stream
-export const DEFAULT_MIMETYPES_SUPPORT = '.(webp|avif|png|gif|jpeg|jpg)'
+export const DEFAULT_MIMETYPES_SUPPORT = /^image\/(webp|avif|png|gif|jpeg|jpg)$/;
 
 export type ImagePipeValidatorOptions = {
   maxSize?:number;
@@ -10,6 +10,6 @@ export type ImagePipeValidatorOptions = {
 }
 
 export const ImagePipeValidator = (options?:ImagePipeValidatorOptions) => new ParseFilePipeBuilder()
-.addFileTypeValidator({ fileType: options?.mimeTypes ? `.(${options.mimeTypes.join('|')})` : DEFAULT_MIMETYPES_SUPPORT })
+.addFileTypeValidator({ fileType: options?.mimeTypes ? new RegExp(`^image\\/(${options.mimeTypes.join("|")})$`) : DEFAULT_MIMETYPES_SUPPORT })
 .addMaxSizeValidator({ maxSize: ( options?.maxSize ?? DEFAULT_MAX_IMAGE_SIZE ) * 1e6 })
 .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY })
