7ddbf9aa9b98532fbeeaf7e68a6a38cd4ad42c7b
Roberto Colabene <robertocolabene@maior.team>
2025-11-27 11:45:30 +0100

    dev

--- lib/pipes/image-compression.pipe.ts
+++ lib/pipes/image-compression.pipe.ts
@@ -37,7 +37,7 @@ export class ImageCompressionPipe implements PipeTransform<Express.Multer.File, 
       file.buffer = compressedBuffer
       file.mimetype = 'image/webp'
       file.size = compressedBuffer.length
-      file.originalname = file.originalname.replace(extname(file.originalname), '.webp').replaceAll(' ','-')
+      file.originalname = this.sanitizeFileName(file.originalname).replace(extname(file.originalname), '.webp')
 
       return file
 
@@ -50,5 +50,17 @@ export class ImageCompressionPipe implements PipeTransform<Express.Multer.File, 
     if(!width || !height) return 70;
     const area = width * height;
     return area > 2e6 ? 50 : area > 1e6 ? 60 : 70;
+  }
+
+  private sanitizeFileName(name: string) {
+    const decoded = decodeURIComponent(name);
+    return decoded
+      .trim()
+      .toLowerCase()
+      .normalize("NFD").replace(/[\u0300-\u036f]/g, '') // no accent
+      .replace(/\s+/g, '-')                             // no space
+      .replace(/[^a-z0-9\.-]/g, '-')                    // no special except dot and dash
+      .replace(/-+/g, '-')                              // no multiple dashes
+      .replace(/^\-+|\-+$/g, '');                       // trim dashes from start and end
   }
 }
