461ebdcf6070de449affad1a48efe2a49bb53c32
Roberto Colabene <robertocolabene@maioradv.com>
2026-03-11 13:05:06 +0100

    dev

--- lib/dto/clauses/model.ts
+++ lib/dto/clauses/model.ts
@@ -8,8 +8,10 @@ export interface DefaultClausesI {
   maxCreatedAt?:Date;
   minUpdatedAt?:Date;
   maxUpdatedAt?:Date;
+  metafields?:Record<string,string>[],
+  translations?:Record<string,string>[],
 }
 
-export type WhereClausesOf<T, P extends keyof T = Exclude<keyof T,keyof DefaultClausesI | 'metafields' | 'translations'>> = {
+export type WhereClausesOf<T, P extends keyof T = Exclude<keyof T,keyof DefaultClausesI>> = {
   [K in P]?: T[K] | T[K][]
 }
--- lib/dto/clauses/special-clauses.decorator.ts
+++ lib/dto/clauses/special-clauses.decorator.ts
@@ -3,21 +3,43 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 import { plainToInstance, Transform, Type } from "class-transformer";
 import { IsArray, IsOptional, ValidateNested } from "class-validator";
 
-export const IsMetafieldClause = <T>(dto: TypeI<T>) => {
+export const IsMetafieldClause = () => {
   return applyDecorators(
     ApiPropertyOptional({
-      type:String,
-      description: 'A key:value couple or a comma-separated list of couples. Format: key1:value1,key2:value2'
+      type: String,
+      description: 'A key:value couple or a comma-separated list of couples. Format: key1:value1,key2:,:value3,:'
     }),
     Transform(({ value }: { value: string }) => 
-      plainToInstance(dto, value.split(',').map(pair => {
+      value.split(',').map(pair => {
         const [key, val] = pair.split(':');
-        return { key: key.trim(), value: val.trim() };
-      }))
+        const obj: Record<string, string> = {};
+        if (key?.trim()) obj.key = key.trim();
+        if (val?.trim()) obj.value = val.trim();
+        return obj;
+      })
     ),
     IsOptional(),
     IsArray(),
-    ValidateNested({ each: true }),
-    Type(() => dto)
+  );
+};
+
+export const IsTranslationClause = () => {
+  return applyDecorators(
+    ApiPropertyOptional({
+      type: String,
+      description: 'A key:locale:value tuple or a comma-separated list of tuples. Format: key1:locale1:value1,key2:locale2:value2'
+    }),
+    Transform(({ value }: { value: string }) => 
+      value.split(',').map(pair => {
+        const [key, locale, val] = pair.split(':');
+        const obj: Record<string, string> = {};
+        if (key?.trim()) obj.key = key.trim();
+        if (locale?.trim()) obj.locale = locale.trim();
+        if (val?.trim()) obj.value = val.trim();
+        return obj;
+      })
+    ),
+    IsOptional(),
+    IsArray(),
   );
 };
